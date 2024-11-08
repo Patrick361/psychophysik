fff6a5c41f3f8ae4227ba25fb4f5d1695f763aad
Patrick361 <paperzzz.lp@gmail.com>
2024-11-08 20:28:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = "https://rzmrgpjrsgilyzobxqgq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6bXJncGpyc2dpbHl6b2J4cWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5MTAyMjQsImV4cCI6MjA0NjQ4NjIyNH0.xL7o-2IqAbUUr7lpVOmNhUgXUMREtRa6q9gyWVb5i60";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let userName = ''; 
